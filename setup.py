69b8db02be8efd027fd901a1a95433c9c47bac4e
Hao Wu <echowuhao@gmail.com>
2017-04-07 11:07:10 -0700

    add test

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='hcvb_lab',
-      version='0.0.2',
+      version='0.0.3',
       description='util in hcvb lab',
       author='Hao Wu',
       author_email='echowuhao@gmail.com',
---
+++ test_hcvb_lab.py
@@ -0,0 +1,20 @@
+import unittest
+import hcvb_lab
+
+class HcvbLabTest(unittest.TestCase):
+    def test_is_bcr_01(self):
+        return(hcvb_lab.is_bcr('105_a_110_a_CSF_UNS_NA_BCR_IgM'))
+
+    def test_is_bcr_02(self):
+        return(hcvb_lab.is_bcr('10311_c_UNS_1000000_IGG_IGVH'))
+
+    def test_is_bcr_03(self):
+        self.assertFalse(hcvb_lab.is_bcr('10311_c_PB_T_CD8_817944_TCR_vb'))
+
+
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
