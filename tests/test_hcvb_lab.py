d1c91cc6c22ef2a1eca0bfc84ef2cf04865f3951
Hao Wu <echowuhao@gmail.com>
2017-05-03 11:29:04 -0700

    add tests

--- tests/test_hcvb_lab.py
+++ tests/test_hcvb_lab.py
@@ -1,5 +1,11 @@
 import unittest
+import os
 from context import hcvb_lab
+from os.path import dirname, join
+
+current_dir = os.path.dirname(__file__)
+
+
 
 class HcvbLabTest(unittest.TestCase):
     def test_is_bcr_01(self):
@@ -10,7 +16,10 @@ class HcvbLabTest(unittest.TestCase):
 
     def test_is_bcr_03(self):
         self.assertFalse(hcvb_lab.is_bcr('10311_c_PB_T_CD8_817944_TCR_vb'))
-
+    def test_blank_file(self):
+        return(hcvb_lab.line_number(join(dirname(current_dir), 'requirements.txt')) == 0)
+    def test_realfile_file(self):
+        return(hcvb_lab.line_number(os.path.join(current_dir, 'context.py')) == 4)
 
 
 if __name__ == '__main__':
